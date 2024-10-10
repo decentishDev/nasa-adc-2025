c2ab34ad71c6eb5b32ecf132bcdffe05cb455d9d
Sharul Shah <sharulshah@icloud.com>
2024-10-10 10:00:06 -0500

    Update GravityHandler.cs

--- Assets/GravityHandler.cs
+++ Assets/GravityHandler.cs
@@ -1,1 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class ExampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("CSC");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
