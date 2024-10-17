e153346e9ea65bf7f90c23f74e4b35a5addf2bf9
decentishDev <decentishdev@gmail.com>
2024-10-16 19:49:06 -0500

    Adding data rate for satellites

--- Assets/SimVars.cs
+++ Assets/SimVars.cs
@@ -7,6 +7,7 @@ using TMPro;
 
 public class SimVars : MonoBehaviour {
     public TextAsset csvFile;
+    public TextAsset csvFileExtra;
     public Slider rowSlider;
     public GameObject speedInputMenu;
     public TMP_InputField speedInputField;
@@ -26,19 +27,22 @@ public class SimVars : MonoBehaviour {
     public static Vector3 v;
     public static Vector3 lastV;
     public static float m = 0f;
+
     public static float wpsa = 0f;
-    public static float wpsaRange = 0f;
+    public TextMeshProUGUI wpsaText;
     public static float ds54 = 0f;
-    public static float ds54Range = 0f;
+    public TextMeshProUGUI ds54Text;
     public static float ds24 = 0f;
-    public static float ds24Range = 0f;
+    public TextMeshProUGUI ds24Text;
     public static float ds34 = 0f;
-    public static float ds34Range = 0f;
+    public TextMeshProUGUI ds34Text;
+
     public static Vector3 rMoon;
     public static Vector3 vMoon;
     public bool RenderingTrail = true;
 
     private List<string[]> dataRows = new List<string[]>();
+    private List<string[]> dataRowsExtra = new List<string[]>();
     private Vector3[] allR = new Vector3[12981];
     private Vector3[] allMoonR = new Vector3[12981];
     public int currentRow = 0;
@@ -48,12 +52,15 @@ public class SimVars : MonoBehaviour {
 
     void Start(){
         string[] data = csvFile.text.Split(new char[] {'\n'});
+        string[] dataExtra = csvFileExtra.text.Split(new char[] {'\n'});
 
         for (int i = 1; i < data.Length; i++){
             string[] fields = data[i].Split(new char[] {','});
+            string[] fieldsExtra = dataExtra[i].Split(new char[] {','});
             dataRows.Add(fields);
-            Vector3 rPos = new Vector3(float.Parse(fields[1]), float.Parse(fields[2]), float.Parse(fields[3]));
-            Vector3 moonPos = new Vector3(float.Parse(fields[14]), float.Parse(fields[15]), float.Parse(fields[16]));
+            dataRowsExtra.Add(fieldsExtra);
+            Vector3 rPos = new Vector3(float.Parse(fieldsExtra[1]), float.Parse(fieldsExtra[2]), float.Parse(fieldsExtra[3]));
+            Vector3 moonPos = new Vector3(float.Parse(fieldsExtra[14]), float.Parse(fieldsExtra[15]), float.Parse(fieldsExtra[16]));
             allR[i - 1] = rPos / 1000f;
             allMoonR[i - 1] = moonPos / 1000f;
         }
@@ -70,18 +77,68 @@ public class SimVars : MonoBehaviour {
     void UpdateRow(float value) {
         currentRow = Mathf.FloorToInt(value);
         time = float.Parse(dataRows[currentRow][0]);
-        r = new Vector3(float.Parse(dataRows[currentRow][1]), float.Parse(dataRows[currentRow][2]), float.Parse(dataRows[currentRow][3]));
-        v = new Vector3(float.Parse(dataRows[currentRow][4]), float.Parse(dataRows[currentRow][5]), float.Parse(dataRows[currentRow][6]));
+        r = new Vector3(float.Parse(dataRowsExtra[currentRow][1]), float.Parse(dataRowsExtra[currentRow][2]), float.Parse(dataRowsExtra[currentRow][3]));
+        v = new Vector3(float.Parse(dataRowsExtra[currentRow][4]), float.Parse(dataRowsExtra[currentRow][5]), float.Parse(dataRowsExtra[currentRow][6]));
         if(currentRow != 0){
-            lastV = new Vector3(float.Parse(dataRows[currentRow - 1][4]), float.Parse(dataRows[currentRow - 1][5]), float.Parse(dataRows[currentRow - 1][6]));
+            lastV = new Vector3(float.Parse(dataRowsExtra[currentRow - 1][4]), float.Parse(dataRowsExtra[currentRow - 1][5]), float.Parse(dataRowsExtra[currentRow - 1][6]));
         }else{
             lastV = v;
         }
-        rMoon = new Vector3(float.Parse(dataRows[currentRow][14]), float.Parse(dataRows[currentRow][15]), float.Parse(dataRows[currentRow][16]));
-        vMoon = new Vector3(float.Parse(dataRows[currentRow][17]), float.Parse(dataRows[currentRow][18]), float.Parse(dataRows[currentRow][19]));
+        rMoon = new Vector3(float.Parse(dataRowsExtra[currentRow][14]), float.Parse(dataRowsExtra[currentRow][15]), float.Parse(dataRowsExtra[currentRow][16]));
+        vMoon = new Vector3(float.Parse(dataRowsExtra[currentRow][17]), float.Parse(dataRowsExtra[currentRow][18]), float.Parse(dataRowsExtra[currentRow][19]));
 
         PositionText.text = $"Position: ({r.x}, {r.y}, {r.z})";
         VelocityText.text = $"Velocity: <{v.x}, {v.y}, {v.z}>";
+
+        if(float.Parse(dataRows[currentRow][8]) == 1f){
+            wpsaText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            wpsa = CalculateLinkBudget(12, float.Parse(dataRows[currentRow][9]));
+            wpsaText.text = "WSPA data rate: " + wpsa.ToString() + " kbps" + ((wpsa > 10000) ? " (max 10,000 kbps)" : "");
+        }else{
+            wpsaText.color = new Color(1.0f, 1.0f, 1.0f, 0.35f);
+            wpsa = 0f;
+            wpsaText.text = "WSPA unavailable";
+        }
+        if(float.Parse(dataRows[currentRow][10]) == 1f){
+            ds54Text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            ds54 = CalculateLinkBudget(34, float.Parse(dataRows[currentRow][11]));
+            ds54Text.text = "DS54 data rate: " + ds54.ToString() + " kbps" + ((ds54 > 10000) ? " (max 10,000 kbps)" : "");
+        }else{
+            ds54Text.color = new Color(1.0f, 1.0f, 1.0f, 0.35f);
+            ds54 = 0f;
+            ds54Text.text = "DS54 unavailable";
+        }
+        if(float.Parse(dataRows[currentRow][12]) == 1f){
+            ds24Text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            ds24 = CalculateLinkBudget(34, float.Parse(dataRows[currentRow][13]));
+            ds24Text.text = "DS24 data rate: " + ds24.ToString() + " kbps" + ((ds24 > 10000) ? " (max 10,000 kbps)" : "");
+        }else{
+            ds24Text.color = new Color(1.0f, 1.0f, 1.0f, 0.35f);
+            ds24 = 0f;
+            ds24Text.text = "DS24 unavailable";
+        }
+        if(float.Parse(dataRows[currentRow][14]) == 1f){
+            ds34Text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            ds34 = CalculateLinkBudget(34, float.Parse(dataRows[currentRow][15]));
+            ds34Text.text = "DS34 data rate: " + ds34.ToString() + " kbps" + ((ds34 > 10000) ? " (max 10,000 kbps)" : "");
+        }else{
+            ds34Text.color = new Color(1.0f, 1.0f, 1.0f, 0.35f);
+            ds34 = 0f;
+            ds34Text.text = "DS34 unavailable";
+        }
+        
+        if(wpsa > ds54 && wpsa > ds24 && wpsa > ds34){
+            wpsaText.color = new Color(0.1f, 1.0f, 0.1f, 1.0f);
+        }
+        if(ds54 > wpsa && ds54 > ds24 && ds54 > ds34){
+            ds54Text.color = new Color(0.1f, 1.0f, 0.1f, 1.0f);
+        }
+        if(ds24 > wpsa && ds24 > ds54 && ds24 > ds34){
+            ds24Text.color = new Color(0.1f, 1.0f, 0.1f, 1.0f);
+        }
+        if(ds34 > wpsa && ds34 > ds54 && ds34 > ds24){
+            ds34Text.color = new Color(0.1f, 1.0f, 0.1f, 1.0f);
+        }
     }
 
     void Update(){
