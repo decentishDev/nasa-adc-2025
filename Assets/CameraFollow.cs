a28659ba39e46242a77a5fee595fcfb19215ffe1
decentishDev <decentishdev@gmail.com>
2024-10-23 20:10:17 -0500

    Added better controls for camera tracking, updated button

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CameraFollow : MonoBehaviour
 {
 
     public static float cameraMode = 0f;
-    private float numberOfModes = 2f;
+    public TextMeshProUGUI modeText;
+    private string[] cameraModes = {"Full system", "Zoomed tracking", "Free tracking"};
 
     public Transform spaceship;
-    public float rotationSpeed = 50f; // Speed of rotation around the spaceship
+    public float rotationSpeed = 10f; // Speed of rotation around the spaceship
     public float distance = 75f; // Distance between the camera and the spaceship
     private float currentRotationAngle = 0f; 
 
@@ -21,11 +23,14 @@ public class CameraFollow : MonoBehaviour
     void Update(){
         if(cameraMode == 0f){
             transform.position = Vector3.Lerp(transform.position, new Vector3(-218f, -82f, -235f), 0.05f);
-            currentRotationAngle = 0f; 
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            currentRotationAngle = 0f;
+            // transform.rotation = Quaternion.Euler(0, 0, 0)
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), 0.05f);
 
 
          } else if (cameraMode == 1f) {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(0f, 0f, -75f) + SimVars.r, 0.05f);
+        } else if (cameraMode == 2f) {
          //  transform.position = Vector3.Lerp(transform.position, new Vector3(0f, 0f, -1 * (distanceFromSpaceship)) + SimVars.r, 0.05f);
 
             float horizontalInput = Input.GetAxis("Horizontal");
@@ -37,7 +42,8 @@ public class CameraFollow : MonoBehaviour
             }
 
             Vector3 direction = new Vector3(Mathf.Sin(currentRotationAngle), 0, Mathf.Cos(currentRotationAngle)) * distance;
-            transform.position = spaceship.position + direction;
+            //transform.position = spaceship.position + direction;
+            transform.position = Vector3.Lerp(transform.position, spaceship.position + direction, 0.05f);
 
             transform.LookAt(spaceship.position);
         }
@@ -45,9 +51,11 @@ public class CameraFollow : MonoBehaviour
 
     public void SwitchCameraMode() {
         cameraMode += 1f;
-        if(cameraMode > numberOfModes - 1f){
+        if(cameraMode > cameraModes.Length - 1f){
             cameraMode = 0f;
         }
+
+        modeText.text = "Camera: " + cameraModes[(int) cameraMode];
     }
 }
 
