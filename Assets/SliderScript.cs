d528946cea8324c6bc6b1b20d130805064b7b400
Immortal215 <sharulshah@icloud.com>
2024-11-17 00:23:27 -0600

    reduced unnessecary code

--- Assets/SliderScript.cs
+++ Assets/SliderScript.cs
@@ -9,31 +9,30 @@ public class SliderScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 
     void Start()
     {
-        if (slider == null)
-        {
-            slider = GetComponent<Slider>();
-        }
+        // if (slider == null)
+        // {
+        //     slider = GetComponent<Slider>();
+        // }
 
-        slider.onValueChanged.AddListener(OnSliderValueChanged);
+      //  slider.onValueChanged.AddListener(OnSliderValueChanged);
     }
 
     void Update()
     {
-        
+
     }
 
-    private void OnSliderValueChanged(float value)
-    {
-        if (sliderMoving)
-        {
-            Debug.Log("Slider is moving. Current value: " + value);
-        }
-    }
+    // private void OnSliderValueChanged(float value)
+    // {
+    //     if (sliderMoving)
+    //     {
+    //         Debug.Log("Slider is moving. Current value: " + value);
+    //     }
+    // }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         sliderMoving = true;
-        Debug.Log("Slider start drag detected");
     }
 
     public void OnPointerUp(PointerEventData eventData)
