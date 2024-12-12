68261ca37413995b8dacabf7a45e6274b2e677c0
Efe Ozalp <efeozalp0101@gmail.com>
2024-12-11 21:54:10 -0600

    Philosophical improvements

--- Assets/ExampleScript.cs
+++ Assets/ExampleScript.cs
@@ -7,7 +7,7 @@ public class ExampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Console.WriteLine("Monkey")
+        Console.WriteLine("Monkey typing the entirety of Hamlet... what are the odds")
     }
 
     // Update is called once per frame
